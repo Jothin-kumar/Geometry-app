e461fbd6661f2fdf431170db51eec334cb082de7
Jothin-kumar <bjothinphysics@gmail.com>
2021-09-26 12:02:18 +0530

    First commit

---
+++ scripts/GUI.py
@@ -0,0 +1,188 @@
+from tkinter import *
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showerror
+from shapes import Point, Line, delete_point, delete_line, points
+
+root = Tk()
+mainframe = Frame(master=root)
+menu_bar = Frame(master=mainframe)
+
+
+def show_all_points():
+    for point in points:
+        point.show()
+
+
+def hide_all_points():
+    for point in points:
+        point.hide()
+
+
+variable_view = IntVar()
+variable_view.set(1)
+
+
+def variable_view_button_command():
+    if variable_view.get():
+        show_all_points()
+    else:
+        hide_all_points()
+
+
+variable_view_button = Checkbutton(master=menu_bar, text='Show variable names', variable=variable_view,
+                                   command=variable_view_button_command)
+variable_view_button.pack(side=TOP, anchor=W, expand=False)
+menu_bar.pack(side=TOP, anchor=W, fill=Y)
+split_frame = PanedWindow(master=mainframe)
+
+shape_pane = Frame(master=split_frame)
+
+
+class ShapePane:
+    def __init__(self, shape_name: str, switch_to_this_shape_command):
+        self.mainframe = Frame(master=shape_pane)
+        self.button = Button(master=self.mainframe, text=shape_name, command=switch_to_this_shape_command)
+        self.button.pack(side=TOP, fill=X)
+        self.listbox = Listbox(master=self.mainframe)
+        self.listbox.pack(side=LEFT, fill=Y)
+        self.scrollbar = Scrollbar(master=self.mainframe)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.listbox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.listbox.yview)
+        self.mainframe.pack(side=TOP, expand=False, anchor=W)
+
+    def empty(self):
+        self.listbox.delete(0, END)
+
+    def append(self, string: str):
+        self.listbox.insert(END, string)
+
+    def set_texts(self, shapes: list):
+        self.empty()
+        for shape in shapes:
+            self.append(shape.name)
+
+    def on_listbox_element_switch(self, command):
+        def get_element(event):
+            curselection = event.widget.curselection()
+            if curselection:
+                curselection = int(curselection[0])
+            else:
+                curselection = 0
+            command(event.widget.get(curselection))
+
+        def select_command(event):
+            if not variable_view.get():
+                variable_view.set(1)
+                show_all_points()
+            get_element(event)
+
+        self.listbox.bind('<<ListboxSelect>>', select_command)
+
+
+split_frame.add(shape_pane)
+
+diagram_editor_frame = Frame(split_frame)
+diagram_editor = Canvas(
+    master=diagram_editor_frame,
+    bg='white',
+    width=20000,
+    height=20000,
+    scrollregion=(0, 0, 20000, 20000)
+)
+for x in range(0, 401):
+    for y in range(0, 401):
+        diagram_editor.create_rectangle(x * 50, y * 50, 50 + (x * 50), 50 + (y * 50), outline='lightgreen')
+horizontal_scroll_bar_diagram_editor = Scrollbar(master=diagram_editor_frame, orient=HORIZONTAL)
+horizontal_scroll_bar_diagram_editor.pack(side=BOTTOM, fill=X)
+horizontal_scroll_bar_diagram_editor.config(command=diagram_editor.xview)
+
+vertical_scroll_bar_diagram_viewer = Scrollbar(master=diagram_editor_frame, orient=VERTICAL)
+vertical_scroll_bar_diagram_viewer.pack(side=RIGHT, fill=Y)
+vertical_scroll_bar_diagram_viewer.config(command=diagram_editor.yview)
+
+diagram_editor.config(
+    xscrollcommand=horizontal_scroll_bar_diagram_editor.set, yscrollcommand=vertical_scroll_bar_diagram_viewer.set)
+diagram_editor.pack(expand=True, fill=BOTH)
+create_line = diagram_editor.create_line
+create_text = diagram_editor.create_text
+delete = diagram_editor.delete
+
+
+def on_diagram_editor_enter(command):
+    diagram_editor.bind('<Enter>', command)
+
+
+def on_diagram_editor_leave(command):
+    diagram_editor.bind('<Leave>', command)
+
+
+def on_diagram_viewer_click(command):
+    diagram_editor.bind('<Button-1>', command)
+
+
+split_frame.add(diagram_editor_frame, minsize=1400)
+
+
+def get_variable_name_from_user():
+    return askstring(title='Rename variable', prompt='Please enter a variable name', parent=root)
+
+
+property_panel = Frame(master=split_frame)
+
+
+class PointPropertyPane:
+    def __init__(self, point: Point, refresh_command):
+        self.mainframe = Frame(master=property_panel)
+        name_label = Label(master=self.mainframe, text=f'Point: {point.name}')
+        name_label.pack()
+
+        def rename():
+            user_variable = get_variable_name_from_user()
+            if str(user_variable).replace(' ', '') and user_variable:
+                try:
+                    point.rename(str(user_variable), refresh_command)
+                    name_label['text'] = f'Point: {point.name}'
+                except ValueError as error_message:
+                    showerror('Error while renaming', error_message)
+
+        rename_button = Button(master=self.mainframe, text='Rename point', bg='snow', fg='black', command=rename)
+        rename_button.pack()
+
+        def delete_point_():
+            delete_point(point)
+            refresh_command()
+            self.delete()
+
+        delete_button = Button(master=self.mainframe, text='Delete point', bg='red', fg='snow', command=delete_point_)
+        delete_button.pack()
+        self.mainframe.pack(side=TOP, anchor=E, expand=False)
+
+    def delete(self):
+        self.mainframe.pack_forget()
+
+
+class LinePropertyPane:
+    def __init__(self, line: Line, refresh_command):
+        self.mainframe = Frame(master=property_panel)
+        name_label = Label(master=self.mainframe, text=f'Line: {line.name}')
+        name_label.pack()
+
+        def delete_line_():
+            delete_line(line)
+            refresh_command()
+            self.delete()
+
+        delete_button = Button(master=self.mainframe, text='Delete line', fg='snow', bg='red', command=delete_line_)
+        delete_button.pack()
+        self.mainframe.pack(side=TOP, anchor=E, expand=False)
+
+    def delete(self):
+        self.mainframe.pack_forget()
+
+
+split_frame.add(property_panel)
+
+split_frame.pack(fill=BOTH)
+mainframe.pack(fill=BOTH)
+mainloop = root.mainloop
---
+++ scripts/main.py
@@ -0,0 +1,109 @@
+import GUI
+import shapes
+
+current_shape = 'point'  # Current shape to edit. Ex: Point, Line
+previous_click_point = None  # Previously clicked coordinates as shapes.Point object. Defined as None for now
+
+
+def switch_to_point_edit():  # Switch to point edit.
+    global current_shape
+    current_shape = 'point'
+
+
+def switch_to_line_edit():  # Switch to Line edit.
+    global previous_click_point
+    global current_shape
+    current_shape = 'line'
+    previous_click_point = None  # Empty previous click point to start a new shape.
+
+
+def refresh_points_panel():  # Command to refresh points.
+    points_pane.set_texts(shapes.points)
+
+
+def refresh_lines_panel():  # Command to refresh lines.
+    lines_pane.set_texts(shapes.lines)
+
+
+def refresh_all():  # Command to refresh side panel.
+    refresh_points_panel()
+    refresh_lines_panel()
+
+
+points_pane = GUI.ShapePane(shape_name='Points', switch_to_this_shape_command=switch_to_point_edit)  # The points panel.
+lines_pane = GUI.ShapePane(shape_name='Lines', switch_to_this_shape_command=switch_to_line_edit)  # The lines panel.
+previous_highlighted_point = None  # Variable to store previous highlighted point.
+previous_highlighted_line = None  # Variable to store previous highlighted line.
+previous_point_property = None  # Variable to store previous point shown in property panel.
+previous_line_property = None  # Variable to store previous line shown in property panel.
+
+
+def on_point_pane_element_switch(string: str):
+    global previous_highlighted_point
+    global previous_point_property
+    if previous_highlighted_point:  # If previous highlighted point is not None.
+        previous_highlighted_point.un_highlight()  # Un-Highlight.
+    if previous_point_property:  # If previous point property is not None.
+        previous_point_property.delete()  # Delete it.
+    point = shapes.get_point_by_name(string)  # Get point object from it's name.
+    if point:
+        point.highlight()  # Highlight the point.
+    previous_point_property = GUI.PointPropertyPane(point, refresh_all)  # When called next time, this is previous.
+    previous_highlighted_point = point  # When called next time, this point is the previously highlighted one.
+
+
+def on_line_pane_element_switch(string: str):
+    global previous_highlighted_line
+    global previous_line_property
+    if previous_highlighted_line:  # If previous highlighted line is not None.
+        previous_highlighted_line.un_highlight()  # Un-Highlight.
+    if previous_line_property:  # If previous line property is not None.
+        previous_line_property.delete()  # Delete it.
+    line = shapes.get_line_by_name(string)  # Get line object from it's name.
+    if line:
+        line.highlight()  # highlight the line.
+    previous_line_property = GUI.LinePropertyPane(line, refresh_all)
+    previous_highlighted_line = line  # When called next time, this line is the previously highlighted one.
+
+
+points_pane.on_listbox_element_switch(on_point_pane_element_switch)
+lines_pane.on_listbox_element_switch(on_line_pane_element_switch)
+
+
+def on_diagram_editor_click(event):  # When user clicks on the diagram editor.
+    x = (int(event.x / 50) * 50) + 25
+    y = (int(event.y / 50) * 50) + 25
+    if current_shape == 'point':  # If current shape is Point, Just create a new point where clicked.
+        global previous_point_property
+        global previous_highlighted_point
+        try:  # Try to create a Point.
+            shapes.point(x, y, GUI.create_text, GUI.delete)  # Create a point.
+            points_pane.set_texts(shapes.points)  # Refresh panel.
+        except ValueError:  # In case it already exists, do nothing.
+            pass
+        if previous_highlighted_point:  # If previous highlighted point is not None.
+            previous_highlighted_point.un_highlight()  # Un-Highlight.
+        if previous_point_property:  # If previous point property is not None.
+            previous_point_property.delete()  # Delete it.
+        point = shapes.get_point_by_coordinates(x, y)  # Get point object from it's coordinates.
+        if point:
+            point.highlight()  # Highlight the point.
+        previous_point_property = GUI.PointPropertyPane(point, refresh_all)  # When called next time, this is previous.
+        previous_highlighted_point = point  # When called next time, this point is the previously highlighted one.
+    elif current_shape == 'line':  # If current shape is line, Draw line from previous clicked coordinate to here.
+        global previous_click_point
+        current_click_point = shapes.get_point_by_coordinates(x, y)
+        if previous_click_point and current_click_point:
+            # If the Point 'previous_click_point' and 'current_click_point' exists.
+            try:  # Try to create a line.
+                shapes.line(previous_click_point, current_click_point, GUI.create_line, GUI.delete)  # Create a line.
+                lines_pane.set_texts(shapes.lines)  # Refresh panel.
+            except ValueError:  # In case it already exists, do nothing.
+                pass
+        previous_click_point = current_click_point
+
+
+GUI.on_diagram_viewer_click(on_diagram_editor_click)
+GUI.mainloop()
+for line in shapes.lines:
+    print(line.name, line.point1.x + 25, line.point1.y + 25, line.point2.x + 25, line.point2.y + 25)
