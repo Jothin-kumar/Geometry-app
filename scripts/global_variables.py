fe2c43a41b37e249ed57063fe71571447bdd0ba4
B.Jothin kumar <bjothinphysics@gmail.com>
2022-01-16 00:17:10 +0530

    Update website.

--- scripts/global_variables.py
+++ scripts/global_variables.py
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Author: Jothin kumar (https://jothin-kumar.github.io/)
+Author: Jothin kumar (https://jothin.tech)
 Github repository of this project: https://github.com/Jothin-kumar/Geometry-app
 """
 dictionary = {}
